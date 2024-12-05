5186fd687e4dbac8709ae0c69cfbaf5218cd4e92
Mukul Sharma <mukulsharma262002@gmail.com>
2024-12-05 23:03:24 +0530

    Added metaverse component in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Hero from "./components/Hero";
+import Metaverse from "./components/Metaverse";
 
 function App() {
   return (
     <main>
       <Hero />
+      <Metaverse />
     </main>
   );
 }
