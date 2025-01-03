aa33f320f19be5f39b1c5a3b302d35fc4d7a98a7
Mukul Sharma <mukulsharma262002@gmail.com>
2025-01-04 00:02:41 +0530

    Added navigation links in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Footer: React.FC = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <div className="container">
+        <div className="text-white py-[90px] max-w-[1100px] mx-auto flex gap-[77px]">
+          <div className="w-full">
+            <ul className="flex justify-around w-full [&_li]:text-[28px]">
+              <li className="cursor-pointer hover:underline">About Us</li>
+              <li className="cursor-pointer hover:underline">Services</li>
+              <li className="cursor-pointer hover:underline">Contact</li>
+              <li className="cursor-pointer hover:underline">Products</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
