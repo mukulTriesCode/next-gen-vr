235ebc57f47defca75499f29a7d7266650ac78d4
Mukul Sharma <mukulsharma262002@gmail.com>
2025-01-07 19:12:38 +0530

    Initiazed AOS in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import "./App.css";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from "react";
 import Experience from "./components/Experience";
 import Footer from "./components/Footer";
 import Gallery from "./components/Gallery";
@@ -7,6 +10,9 @@ import Metaverse from "./components/Metaverse";
 import Subscribe from "./components/Subscribe";
 
 function App() {
+  useEffect(()=>{
+    AOS.init();
+  },[])
   return (
     <main>
       <Hero />
